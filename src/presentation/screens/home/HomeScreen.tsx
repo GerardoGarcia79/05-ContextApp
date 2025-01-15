13234f53e4dc703e13916bd830395163ddfb234b
Gerardo De Leon Garcia <gerardo-leon10@hotmail.com>
2025-01-15 12:59:23 -0600

    Initial configurations

---
+++ src/presentation/screens/home/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+import {styles} from '../../../config/app-theme';
+
+export const HomeScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>HomeScreen</Text>
+    </View>
+  );
+};
